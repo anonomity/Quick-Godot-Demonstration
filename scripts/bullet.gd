b9a01c8950a25b0c9cc70a6b0067c3bf3ce86361
Jackie <jlauravelez@gmail.com>
2023-11-01 18:47:14 +0100

    before upgrade

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -5,3 +5,12 @@ var speed = 200
 
 func _process(delta):
 	position += transform.x * speed * delta
+
+
+func _on_body_entered(body):
+	body.die()
+	queue_free()
+
+
+func _on_timer_timeout():
+	queue_free()
--- scripts/dragon_fly.gd
+++ scripts/dragon_fly.gd
@@ -8,10 +8,13 @@ extends CharacterBody2D
 @onready var splat = $splat
 @onready var animation_player = $AnimationPlayer
 
-
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var past_x = null
+var is_alive = true
 
 func _physics_process(delta):
+	
+#	print(animated_sprite_2d.animation)
 	if past_x != null:
 		if global_position.x <= past_x:
 			animated_sprite_2d.flip_h = false
@@ -20,11 +23,22 @@ func _physics_process(delta):
 
 	past_x = global_position.x
 	
-	if path_follow:
+	if path_follow and is_alive:
 		path_follow.progress_ratio +=  speed * delta
+	
+	if !is_alive:
+		velocity.y += delta * gravity
+
 
 func die():
-	animation_player.play("die")
+	if is_alive:
+		print("dying")
+		dead_dragonfly()
+		animation_player.play("die")
+		animated_sprite_2d.play("dead")
 	
-func delete():
-	queue_free()
+
+
+func dead_dragonfly():
+	is_alive = false
+	
